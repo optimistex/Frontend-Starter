554f359cea322e6ff5ae135242c716960167b7bc
optimistex <optimistex@gmail.com>
2022-05-22 10:22:12 +0300

    update: UI

--- frontend/src/app/feature-ui/cart/cart.module.ts
+++ frontend/src/app/feature-ui/cart/cart.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { PriceModule } from '@fe-core-ui/price/price.module';
 import { ProductModule } from '@fe-feature-api/product/product.module';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports: [CommonModule, PriceModule, ProductModule],
+  imports: [CommonModule, RouterModule, PriceModule, ProductModule],
   declarations: [CartComponent, CartItemComponent],
   exports: [CartComponent],
 })
