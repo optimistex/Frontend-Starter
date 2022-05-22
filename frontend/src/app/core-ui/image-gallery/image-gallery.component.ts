cd7b0a22ad4d59673a39aed33cbf676a32926498
optimistex <optimistex@gmail.com>
2022-05-22 10:39:50 +0300

    update: implement gallery

--- frontend/src/app/core-ui/image-gallery/image-gallery.component.ts
+++ frontend/src/app/core-ui/image-gallery/image-gallery.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '@fe-core/models/product';
+
+type ImageGalleryData = Pick<Product, 'defaultImage' | 'images'>;
 
 @Component({
-  selector: 'app-image-gallery',
+  selector: 'app-image-gallery[data]',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.scss'],
 })
 export class ImageGalleryComponent {
+  @Input() public data!: ImageGalleryData;
 }
--- frontend/src/app/pages/product-page/product-page.module.ts
+++ frontend/src/app/pages/product-page/product-page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ImageGalleryModule } from '@fe-core-ui/image-gallery/image-gallery.module';
+import { PriceModule } from '@fe-core-ui/price/price.module';
 import { ProductPageRoutingModule } from './product-page-routing.module';
 import { ProductPageComponent } from './product-page.component';
 
 @NgModule({
-  imports: [CommonModule, ProductPageRoutingModule],
+  imports: [CommonModule, ProductPageRoutingModule, ImageGalleryModule, PriceModule],
   declarations: [ProductPageComponent],
 })
 export class ProductPageModule {
